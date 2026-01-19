d64ca7e4f3dacfa71e423d823f621f76c7481abc
Alejandro Diest <alejandrodiestsanz@gmail.com>
2026-01-19 20:20:17 +0100

    Minimapa

---
+++ scenes/UI/Minimapa.gd
@@ -0,0 +1,19 @@
+extends SubViewportContainer
+
+@onready var camara_mapa = $SubViewport/Camera2D
+@onready var sub_viewport = $SubViewport
+var jugador: Node2D = null
+
+func _ready():
+	
+	sub_viewport.world_2d = get_viewport().world_2d
+	
+func _process(delta):
+	if not is_instance_valid(jugador):
+		jugador = get_tree().get_first_node_in_group("Jugador")
+		return
+
+	# DEBUG: Imprime dónde está la cámara
+	print("Cámara Mapa en: ", jugador.global_position) 
+	
+	camara_mapa.global_position = jugador.global_position
